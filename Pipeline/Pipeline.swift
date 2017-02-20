af3d80e11408e4df9cd8c9d48892bbd96b75a83b
Joe Meineke <joe.meineke@gmail.com>
2017-02-20 10:13:46 -0500

    initial check in

---
+++ Pipeline/Pipeline.swift
@@ -0,0 +1,15 @@
+//
+//  Pipeline.swift
+//  Pipeline
+//
+//  Created by Joe Meineke on 2/20/17.
+//  Copyright © 2017 com.joemeineke. All rights reserved.
+//
+
+import Foundation
+
+class Pipeline {
+    func display() -> String {
+        return "hello"
+    }
+}
